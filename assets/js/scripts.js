7a4a64db876f6d50b67b90c83bbdeac435e844e3
Jamesgit22 <jschroeder97@icloud.com>
2023-03-29 18:33:57 -0500

    created new variables for each question and a question one object

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,6 +14,17 @@ startQuiz.addEventListener("click", startfunction);
 // This function will start the quiz and be called when the start quiz button is clicked
 function startfunction() {
     
+    // Before getting to the logic portion, lets create an object that contains the info for question one.
+
+    // const questionOneObject = {
+    //     name:questionOneObject,
+    //     answerOne: "Place Holder One",
+    //     answerTwo: "place holder two",
+    //     answerThree: "place holder three",
+    //     answerFour: "correct answer"
+    // }
+
+
     // First I will hide the start card
     startCard.setAttribute("style", "display: none;");
 
@@ -29,8 +40,18 @@ function startfunction() {
     // Now lets give this new element some children to add content to.
     // Todo this, we first need to creat them and then we can add the content.
 
-    let questionOneQuestion = document.createElement("div")
+    let questionOneContainer = document.createElement("div");
+    let questionOneQuestion = document.createElement('h2');
+    let questionOneAnswers = document.createElement("ol");
+    let questionOneAnswerOne = document.createElement("li");
+    let questionOneAnswerTwo = document.createElement("li");
+    let questionOneAnswerThree = document.createElement("li");
+    let questionOneAnswerFour = document.createElement("li");
 
+    // Now we can assign content to each new element.
+    questionOneQuestion.innerText = "this is a test";
+    // document.body.appendChild(questionOneQuestion);
+//  This needs more work
     questionOneQuestion.className = "question-text";
 
 
