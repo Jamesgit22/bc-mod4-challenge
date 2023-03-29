6b6d864d9d3c2e5a9b56bcdbb1a3cc61d028e0fb
Jamesgit22 <jschroeder97@icloud.com>
2023-03-29 17:54:28 -0500

    styled the first start screen with css and assigned a variable in javascript for the start button.

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+// first I want to make a listener event for the start quiz button
+
+// Todo this, I need to make a variable and store the start quiz button in it so that I can make changes to it.
+
+const startQuiz = document.getElementById("start-quiz-btn");
+
+
+// Now I can add a listener event.
